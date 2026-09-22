ff1aba2de6d0e6ea1ebe934fedeb3986255a2a34
hestia33 <parisashaddel333@gmail.com>
2026-09-22 13:59:35 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='hestia-oracle-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>{if(e.request.url.startsWith(self.location.origin))e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
